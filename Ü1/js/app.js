db33f58b47c6a5b486837c39eef05e782526a6f3
Leon <le.on@outlook.com>
2016-10-19 22:13:27 +0200

    JS implementiert
    - JS Code von Tobi übernommen
    - Klicken eines Buttons ändert nun auch sein Aussehen durch Wechsel der
    Klasse
    - Klicken eines Buttons ändert nun auch die Benennung

--- Ü1/js/app.js
+++ Ü1/js/app.js
@@ -1,13 +1,51 @@
-document.getElementById("play1").addEventListener("click", playVid);
-document.getElementById("pause1").addEventListener("click", pauseVid); 
+/**
+* @author: Leon Rösler
+* @author: Marlon Mattern
+* @author: Tobias Klatt
+* Datei: app.js
+* Datum: 16.10.2016
+* 
+* This JavaScript-Programm initialises the functionality of our website.
+* There is a button for every video.
+* Within the following code buttons and videos get connected,
+* so a user can start or pause a video by clicking the right button.
+*/
 
-var vid = document.getElementById("video1");
+(function () {
+    window.addEventListener("load", connectButtonsWithVideos, false);
 
+    function connectButtonsWithVideos() {
+        var buttonArray = document.getElementsByTagName("button");
+        var videoArray = document.getElementsByTagName("video");
 
-function playVid() { 
-    vid.play(); 
-} 
+        for (i = 0; i < buttonArray.length; i++) {
+            (function () {
+                var button = buttonArray[i];
+                var video = videoArray[i];
+                button.addEventListener("click", function() {
+                    playOrPauseVideoDependingOnStatus(video, button);
+                    switchButton();
+                   });
+            })();
+        }
+    }
 
-function pauseVid() { 
-    vid.pause(); 
-}
+    function playOrPauseVideoDependingOnStatus(video, button) {
+        var videoIsPaused = video.paused;
+        if (videoIsPaused) {
+            video.play();
+            button.classList.add("btnPause");
+            button.innerHTML = "PAUSE";
+            button.classList.remove("btnPlay");
+        } else {
+            video.pause();
+            button.classList.add("btnPlay");
+            button.innerHTML = "PLAY";
+            button.classList.remove("btnPause");
+        }
+    }
+    
+    function switchButton(){
+        console.log("Test");
+    }
+})();
