2a87087c40cc3a4dc2fd05ec5f69b90dfe0df12e
Leon <le.on@outlook.com>
2016-11-07 22:47:10 +0100

    5a zT
    file.txt wird ausgelesen und Inhalt dargestellt

--- Ü2/app.js
+++ Ü2/app.js
@@ -1,24 +1,35 @@
 var express = require('express');
 var app = express();
 var timestamp = new Date();
-
+var fileSystem = require('fs');
 
 app.use('/public', express.static('static'));
 
-
 app.get('/time' , function(req, res) {
 	res.set('Content-Type', 'text/plain');
-	res.send(timestamp.getHours() + ":" + timestamp.getMinutes() + ":" + timestamp.getSeconds() );
-	
+	res.send(timestamp.getHours() + ':' + timestamp.getMinutes() + ':' + timestamp.getSeconds());	
 });
-app.get('/*' , function(req, res) {
 
-	
-    res.send('<!DOCTYPE html>' + '<html lang= "en">' + '<head><meta charset="utf-8"></head>' + '<body><h1>Hello world!</h1></body>' + '</html>'
-			 
-             );
+
+app.get('/file.txt', function(request, response){
+    fileSystem.readFile('file.txt', function (err, text) {
+        if (err) return console.log(err);
+        res.set('Content-Type', 'text/plain');
+        res.write(text);
+        res.end();
+    });
+});
+
+app.get('/*' , function(req, res) {
+    res.send('<!DOCTYPE html>' + 
+             '<html lang= "en">' + 
+                '<head> <meta charset="utf-8"> </head>' + 
+                '<body> <h1>Hello world!</h1> </body>' + 
+             '</html>'
+    );
 });
 
 var server = app.listen(3000,function(){
     console.log('helloworld app is ready and listining at http://localhost:3000');
 });
+ 
